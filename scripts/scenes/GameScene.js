b2137d3e068ba622e7855fe364c4b1e9e082f625
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-05-14 18:17:33 +0300

    add method to draw lines

--- scripts/scenes/GameScene.js
+++ scripts/scenes/GameScene.js
@@ -7,6 +7,7 @@ class GameScene extends Phaser.Scene {
     this.createBackground();
     this.createDots();
     this.start();
+    this.graphics = this.add.graphics();
   }
 
   start() {
@@ -40,6 +41,7 @@ class GameScene extends Phaser.Scene {
       }
     }
     this.input.on("gameobjectdown", this.onDotClicked, this);
+    // this.input.on("pointerdown", this.dotSelect, this);
   }
   initDotsPosition() {
     let positions = [];
@@ -66,9 +68,73 @@ class GameScene extends Phaser.Scene {
     this.positions = positions;
   }
 
+  dotSelect(pointer) {
+    // if (pointer.position.x === this.dots[0].position.x) {
+    if (pointer.position.x === this.dots[0].texture.key) {
+      console.log("Nice");
+    }
+    // let pointerPosition = pointer.position.x;
+    console.log(pointer, this);
+
+    // if(this.canPick){
+    //     this.dragging = true;
+    //     let row = Math.floor(pointer.y / gameOptions.gemSize);
+    //     let col = Math.floor(pointer.x / gameOptions.gemSize);
+    //     let pickedGem = this.gemAt(row, col)
+    //     if(pickedGem != -1){
+    //         if(this.selectedGem == null){
+    //             pickedGem.gemSprite.setScale(1.2);
+    //             pickedGem.gemSprite.setDepth(1);
+    //             this.selectedGem = pickedGem;
+    //         }
+    //         else{
+    //             if(this.areTheSame(pickedGem, this.selectedGem)){
+    //                 this.selectedGem.gemSprite.setScale(1);
+    //                 this.selectedGem = null;
+    //             }
+    //             else{
+    //                 if(this.areNext(pickedGem, this.selectedGem)){
+    //                     this.selectedGem.gemSprite.setScale(1);
+    //                     this.swapGems(this.selectedGem, pickedGem, true);
+    //                 }
+    //                 else{
+    //                     this.selectedGem.gemSprite.setScale(1);
+    //                     pickedGem.gemSprite.setScale(1.2);
+    //                     this.selectedGem = pickedGem;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+  }
+
   onDotClicked(pointer, dot) {
     if (dot.texture.key === "dot1") {
-      console.log(dot.texture, "dot clicked");
+      this.graphics.lineStyle(8, 0x2f3b8c);
+      this.graphics.moveTo(dot.x, dot.y);
+      this.graphics.lineTo(dot.x + 64, dot.y);
+      this.graphics.strokePath();
+      console.log(dot, "dot clicked");
+    } else if (dot.texture.key === "dot2") {
+      this.graphics.lineStyle(8, 0x50bd15);
+      this.graphics.moveTo(dot.x, dot.y);
+      this.graphics.lineTo(dot.x, dot.y + 64);
+      this.graphics.strokePath();
+    } else if (dot.texture.key === "dot3") {
+      this.graphics.lineStyle(8, 0x582a7f);
+      this.graphics.moveTo(dot.x, dot.y);
+      this.graphics.lineTo(dot.x, dot.y + 64);
+      this.graphics.strokePath();
+    } else if (dot.texture.key === "dot4") {
+      this.graphics.lineStyle(8, 0xa30c0c);
+      this.graphics.moveTo(dot.x, dot.y);
+      this.graphics.lineTo(dot.x, dot.y + 64);
+      this.graphics.strokePath();
+    } else if (dot.texture.key === "dot5") {
+      this.graphics.lineStyle(8, 0xb39f1e);
+      this.graphics.moveTo(dot.x, dot.y);
+      this.graphics.lineTo(dot.x, dot.y + 64);
+      this.graphics.strokePath();
     }
   }
 
